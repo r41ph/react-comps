63424e557e902adea4c84c8f57e3ac36b3d17a9b
r4lfl4b <dev@rgalan.es>
2019-05-29 20:05:25 +0100

    feat: add propTypes

--- src/lib/CircularNavigation.js
+++ src/lib/CircularNavigation.js
@@ -1,9 +1,39 @@
 import React, { useState } from "react";
 import cx from 'classnames';
+import PropTypes from 'prop-types';
 import "./CircularNavigation.scss";
 
-const CircularNavigation = props => {
+const propTypes = {
+  /**
+   * Controls if an overlay is shown when the nav is open
+   */
+  overlay: PropTypes.bool,
 
+  /**
+   * Sets the overlay color
+   */
+  overlayColor: PropTypes.string,
+
+  /**
+   * Controls if the text under the icons is visible
+   */
+  showText: PropTypes.bool,
+
+  /**
+   * Controls if the icons are visible
+   */
+  showIcon: PropTypes.bool
+}
+
+const defaultProps = {
+  overlay: false,
+  overlayColor: "#000",
+  showText: false,
+  showIcon: true
+}
+
+
+const CircularNavigation = props => {
   const [navOpen, setNavOpen] = useState(false);
 
   const {
@@ -60,5 +90,8 @@ CircularNavigation.item = ({ index, href, icon, children }) => (
     </a>
   </li>
 )
+
+CircularNavigation.propTypes = propTypes;
+CircularNavigation.defaultProps = defaultProps;
 
 export default CircularNavigation;
