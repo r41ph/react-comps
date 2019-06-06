6d44c3bea6698b26db9a0f2be706380b3aed0eee
r4lfl4b <dev@rgalan.es>
2019-06-06 21:38:00 +0100

    feat: add material icons npm

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { render } from "react-dom";
+import HomeIcon from '@material-ui/icons/Home';
+import AnnouncementIcon from '@material-ui/icons/Announcement';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import BuildIcon from '@material-ui/icons/Build';
+import EmailIcon from '@material-ui/icons/Email';
 import {
   CircularNavigation,
   Button
 } from "./lib/";
 
+
 const App = () => (
   <>
     <CircularNavigation overlay={true}>
-      <CircularNavigation.item href={'#'} icon={"home"}>Home</CircularNavigation.item>
-      <CircularNavigation.item href={'#'} icon={"announcement"}>About</CircularNavigation.item>
-      <CircularNavigation.item href={'#'} icon={"assignment"}>Projects</CircularNavigation.item>
-      <CircularNavigation.item href={'#'} icon={"build"}>Labs</CircularNavigation.item>
-      <CircularNavigation.item href={'#'} icon={"email"}>Contact</CircularNavigation.item>
+      <CircularNavigation.item href={'#'} icon={<HomeIcon />}>Home</CircularNavigation.item>
+      <CircularNavigation.item href={'#'} icon={<AnnouncementIcon />}>About</CircularNavigation.item>
+      <CircularNavigation.item href={'#'} icon={<AssignmentIcon />}>Projects</CircularNavigation.item>
+      <CircularNavigation.item href={'#'} icon={<BuildIcon />}>Labs</CircularNavigation.item>
+      <CircularNavigation.item href={'#'} icon={<EmailIcon />}>Contact</CircularNavigation.item>
     </CircularNavigation>
     <Button onClick={() => { console.log('Button clicked') }}>myButton</Button>
   </>
