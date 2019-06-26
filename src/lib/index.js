3664a5d8f499f8f9626b25ac4d8f3f38b5d511d4
r4lfl4b <dev@rgalan.es>
2019-06-26 12:38:14 +0100

    fix: add SelectList

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,9 @@
 import CircularNavigation from "./CircularNavigation/CircularNavigation";
 import Button from "./Button/Button";
+import SelectList from "./SelectList/SelectList";
+
 export {
   CircularNavigation,
-  Button
+  Button,
+  SelectList
 };
