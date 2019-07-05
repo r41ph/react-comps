3a7d1a88840f7d93477c1730d1caf176f02d7335
r4lfl4b <dev@rgalan.es>
2019-07-05 17:04:01 +0100

    story: add icon buttons aria-label

--- src/stories/button.stories.js
+++ src/stories/button.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { Button } from '../lib';
 
-import LaunchIcon from "@material-ui/icons/Launch"
+import LaunchIcon from '@material-ui/icons/Launch';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import EmailIcon from '@material-ui/icons/Email';
 
@@ -14,12 +14,14 @@ storiesOf('Button', module)
       inline: true,
 
       source: true
-    },
+    }
   })
   .add('Types', () => (
     <div className="rc-story story-buttons-types">
       <Button size="md">Button</Button>
-      <Button size="md" href="/" target="_blank">Anchor</Button>
+      <Button size="md" href="/" target="_blank">
+        Anchor
+      </Button>
     </div>
   ))
   .add('Sizes', () => (
@@ -50,15 +52,33 @@ storiesOf('Button', module)
     <div className="rc-story story-buttons-just-icon">
       <h3>Button</h3>
       <div className="buttons">
-        <Button size="sm" icon={LaunchIcon} />
-        <Button size="md" icon={AssignmentIcon} />
-        <Button size="lg" icon={EmailIcon} />
+        <Button size="sm" icon={LaunchIcon} aria-label="Launch" />
+        <Button size="md" icon={AssignmentIcon} aria-label="Assignment" />
+        <Button size="lg" icon={EmailIcon} aria-label="Email" />
       </div>
       <h3>Anchor link</h3>
       <div className="anchors">
-        <Button size="sm" href={"/"} target="_blank" icon={LaunchIcon} />
-        <Button size="md" href={"/"} target="_blank" icon={AssignmentIcon} />
-        <Button size="lg" href={"/"} target="_blank" icon={EmailIcon} />
+        <Button
+          size="sm"
+          href={'/'}
+          target="_blank"
+          icon={LaunchIcon}
+          aria-label="Launch"
+        />
+        <Button
+          size="md"
+          href={'/'}
+          target="_blank"
+          icon={AssignmentIcon}
+          aria-label="Assignment"
+        />
+        <Button
+          size="lg"
+          href={'/'}
+          target="_blank"
+          icon={EmailIcon}
+          aria-label="Email"
+        />
       </div>
     </div>
   ));
