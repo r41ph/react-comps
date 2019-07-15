1ec904e0007ea002a63819e9c730e237cd38df8c
r4lfl4b <dev@rgalan.es>
2019-07-15 17:39:03 +0100

    fix: add input component

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,5 @@
-import Button from "./Button/Button";
-import SelectList from "./SelectList/SelectList";
+import Button from './Button/Button';
+import SelectList from './SelectList/SelectList';
+import Input from './Input/Input';
 
-export {
-  Button,
-  SelectList
-};
+export { Button, SelectList, Input };
