a5c78ad9109b10791ed13c7475e7051ada1754a7
r4lfl4b <dev@rgalan.es>
2019-08-22 17:12:53 +0100

    feat: add tooltip to library

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,6 @@
 import Button from './Button/Button';
 import SelectList from './SelectList/SelectList';
 import Input from './Input/Input';
+import Tooltip from './Tooltip/Tooltip';
 
-export { Button, SelectList, Input };
+export { Button, SelectList, Input, Tooltip };
