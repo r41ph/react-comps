f4930ecb62a688685c26a80bfd60292c0f26b061
r4lfl4b <dev@rgalan.es>
2019-09-25 21:09:04 +0100

    feat: add badge to collection

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,5 +2,6 @@ import Button from './Button/Button';
 import SelectList from './SelectList/SelectList';
 import Input from './Input/Input';
 import Tooltip from './Tooltip/Tooltip';
+import Badge from './Badge/Badge';
 
-export { Button, SelectList, Input, Tooltip };
+export { Button, SelectList, Input, Tooltip, Badge };
