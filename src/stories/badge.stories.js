632a0ec6073794db91d17e1c160dd0578bae38f0
r4lfl4b <dev@rgalan.es>
2019-09-30 22:31:33 +0100

    story: add badge

---
+++ src/stories/badge.stories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { Badge } from '../lib';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+storiesOf('Badge', module)
+  .addDecorator(withInfo)
+  .addParameters({
+    info: {
+      inline: true,
+      source: true
+    }
+  })
+  .add('Size', () => (
+    <div className="rc-story">
+      <Badge text="Blueberry" bgColor="purple" />
+      <br />
+      <Badge text="Banana" bgColor="yellow" />
+      <br />
+      <Badge text="Strawberry" />
+    </div>
+  ))
+  .add('Custom icon', () => (
+    <div className="rc-story">
+      <Badge text="Task one" icon={<AssignmentIcon />} />
+    </div>
+  ))
+  .add('Custom title', () => (
+    <div className="rc-story">
+      <Badge text="Strawberry" title="I'm a badge!" />
+    </div>
+  ));
