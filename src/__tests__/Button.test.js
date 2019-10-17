8a6ed02e293a24e1efdbbb9cc1c89d74956e550e
r4lfl4b <dev@rgalan.es>
2019-10-17 23:21:02 +0100

    test: add button basic tests

--- src/__tests__/Button.test.js
+++ src/__tests__/Button.test.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
 import Button from '../lib/Button/Button';
+import EmailIcon from '@material-ui/icons/Email';
 
 describe('Button component', () => {
-  it('Button on click', () => {
-    const wrapper = mount(<Button>My Button</Button>);
+  it('Button renders children', () => {
+    const wrapper = shallow(<Button>My Button</Button>);
+    expect(wrapper.text()).to.equal('My Button');
+  });
 
-    const button = wrapper.find('button');
-    button.simulate('click');
-    expect(button).toEqual(true);
+  it('Button renders .rc-button-sm CSS class', () => {
+    const wrapper = shallow(<Button size="sm">My Button</Button>);
+    expect(wrapper.find('.rc-button-sm')).to.have.lengthOf(1);
+  });
+
+  it('Button renders .rc-button-md CSS class', () => {
+    const wrapper = shallow(<Button size="md">My Button</Button>);
+    expect(wrapper.find('.rc-button-md')).to.have.lengthOf(1);
+  });
+
+  it('Button renders .rc-button-lg CSS class', () => {
+    const wrapper = shallow(<Button size="lg">My Button</Button>);
+    expect(wrapper.find('.rc-button-lg')).to.have.lengthOf(1);
+  });
+
+  it('Button renders an icon', () => {
+    const wrapper = shallow(
+      <Button size="lg" icon={() => <EmailIcon />} aria-label="Email">
+        My Button
+      </Button>
+    );
+    expect(wrapper.find('.rc-button__icon')).to.have.lengthOf(1);
   });
 });
